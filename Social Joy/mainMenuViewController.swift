e5597a3dba3e75025bf294327526874e15f40291
Trevor A Cohen <cohenta2@egr-maclab-10.local>
2018-05-01 21:25:41 -0400

    Addd json request

--- Social Joy/mainMenuViewController.swift
+++ Social Joy/mainMenuViewController.swift
@@ -14,8 +14,7 @@ class mainMenuViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
 
     override func didReceiveMemoryWarning() {
